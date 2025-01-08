b453fe26cb8c5bbccd84f7ccb05c14ac9fee830e
Jayasree Varadarajan <140917755+jaypython13@users.noreply.github.com>
2025-01-08 20:48:29 +0000

    Update main.py

--- main.py
+++ main.py
@@ -84,11 +84,11 @@ def assess_sample(df, sample_id):
         st.write(f"Degradation Index: {degradation_index}")
 
         if degradation_index < 1:
-            st.success("The sample is not degraded and is ready for further analysis.")
+            st.success("## AI Assesement : The sample is not degraded and is ready for further analysis.")
         elif 1 <= degradation_index < 10:
-            st.warning("The sample is degraded. No action needed.")
+            st.warning("## AI Assesement : The sample is degraded. No action needed.")
         else:
-            st.error("The sample is significantly degraded. Resample is required.")
+            st.error("## AI Assesement : The sample is significantly degraded. Resample is required.")
 
 # ---------------------------
 # Machine Learning Model
@@ -161,14 +161,14 @@ def main():
             st.link_button("\n Click here to view more information", "https://ssg-cic.org/", type = "primary")
         
     if app == "AI Sample Assesement Tool":
-            st.title("AI Assesement Tool for Forensic Sample Analysis")
+            st.title("🤖AI Assesement Tool for Forensic Sample Analysis")
             
             st.info(" \n The AI-powered Assesement Tool is a comprehensive and automated solution for forensic sample quality assessment.\
                         By combining structured decision-making with advanced machine learning techniques, it Saves time and improves efficiency in forensic workflows, provides accurate, reliable assessments.\
                         It also Enhances decision-making with clear, actionable insights.\
                         This tool is an invaluable asset for forensic labs, ensuring quality control and minimizing errors in sample analysis")
-            st.subheader("Assess Sample")
-            sample_id = st.text_input("Enter Sample ID for Analysis")
+            st.subheader("🔍 Assess Sample")
+            sample_id = st.text_input("Enter Sample ID for AI Analysis")
             prepared_data = prepare_data(df)
             if sample_id:
                assess_sample(prepared_data,sample_id)

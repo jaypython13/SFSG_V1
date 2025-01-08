6ad0db0ea0656f5efffea5a11679ed11e6106afd
Jayasree Varadarajan <140917755+jaypython13@users.noreply.github.com>
2025-01-08 18:45:37 +0000

    Update sfsg_ai_tool_streamlit_v1.py

--- sfsg_ai_tool_streamlit_v1.py
+++ sfsg_ai_tool_streamlit_v1.py
@@ -11,7 +11,7 @@ import numpy as np
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report, accuracy_score
-
+from PIL import Image
 # ---------------------------
 # Utility Functions
 # ---------------------------

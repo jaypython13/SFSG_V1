65759cb99919d2543127204a97fa237e0960fd71
Jayasree Varadarajan <140917755+jaypython13@users.noreply.github.com>
2025-01-08 19:45:30 +0000

    Update sfsg_ai_tool_streamlit_v1.py

--- sfsg_ai_tool_streamlit_v1.py
+++ sfsg_ai_tool_streamlit_v1.py
@@ -141,9 +141,9 @@ def main():
                 styles={
                     "container": {"padding": "0!important","background-color":'white'},
                     "icon": {"color": "white", "font-size": "20px"}, 
-                    "menu_title":{"background-color": "#e9ede8"} ,    
+                    "menu_title":{"background-color": "white"} ,    
                     "nav-link": {"color":"white","font-size": "20px", "text-align": "center", "margin":"1px", "--hover-color": "#1e8a06"},
-                    "nav-link-selected": {"background-color": "#e9ede8"},}
+                    "nav-link-selected": {"background-color": "#1c43ba"},}
             )
         
     if app =="About Us":
